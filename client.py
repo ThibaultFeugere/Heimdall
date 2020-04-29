7ed579a8e51dfc868a4b93cc4090dc6aed1e0394
Thibault <thibaultfeugere@gmail.com>
2020-04-29 09:35:57 +0200

    A little bit cleaner

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
 import socket
 
+adresse, port = ("192.168.1.4", 5555)
+
+username = input("Quel est votre pseudo ?")
+
 # Socket donnant l'accès au serveur
 connexion_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-adresse, port = ("192.168.1.4", 5555)
-
-# Demande de connexion au serveur
-connexion_serveur.connect((adresse, port))
-
-# On envoie en bytes un message au serveur
-connexion_serveur.send(str.encode("Bonjour serveur !"))
-
-# On ferme la connexion
-connexion_serveur.close()
+try:
+    # Demande de connexion au serveur
+    connexion_serveur.connect((adresse, port))
+    print("Client connecté")
+    # On envoie en bytes un message au serveur
+    connexion_serveur.sendall(username.encode("utf8"))
+except ConnectionRefusedError:
+    print("Connexion au serveur échouée")
+finally:
+    # On ferme la connexion
+    connexion_serveur.close()
--- server.py
+++ server.py
@@ -7,18 +7,23 @@ socket_ecoute = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 adresse, port = ("192.168.1.4", 5555)
 
 # On lie le socket à une adresse et un port
-socket_ecoute.bind((addresse, port))
+socket_ecoute.bind((adresse, port))
 
-# On met le socket à l'état d'écoute sur le port bindé
-socket_ecoute.listen()
+print("Le serveur est démarré")
 
-# On accepte la connexion du client
-connexion_client, adresse_client = socket_ecoute.accept()
+while True:
+    # On met le socket à l'état d'écoute sur le port bindé
+    socket_ecoute.listen(5)
 
-# On attend de recevoir un message du serveur
-message = connexion_client.recv(1024)
-print(message)
+    # On accepte la connexion du client
+    connexion_client, adresse_client = socket_ecoute.accept()
+
+    # On attend de recevoir un message du serveur
+    message = connexion_client.recv(1024)
+    print(message)
 
 # Fermeture des connexions
 connexion_client.close()
+
+# Fermeture du socket
 socket_ecoute.close()
