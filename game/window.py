f326f7b2ce74d1f08fc5b43b6b1e4323217ae1e9
vivimouret29 <vivien.mouret@ynov.com>
2020-05-11 14:42:03 +0200

    :fire: disable pygame icon

--- game/window.py
+++ game/window.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import os
 import pygame as pg
 import pygame_textinput
-import os.path
 
 
 pg.font.init()
@@ -16,26 +16,30 @@ screen = pg.display.set_mode((800,600))
 
 # title & icone
 pg.display.set_caption("Heimdall")
-#icon = pg.image.load(filepath,'images/heimdall.png').convert()
+#icon = pg.image.load(filepath,'images/heimdall.png') #pygame.error: Unsupported image format
 #pg.display.set_icon(icon)
 
 # text input/object
 textinput = pygame_textinput.TextInput()
 
 # loop game
-while True:
+running = True
+while running:
 
     # color front
-    screen.fill((255, 255, 255))
+    screen.fill((255,255,255))
 
     events = pg.event.get()
     for event in events:
         if event.type == pg.QUIT:
+            running = False
             exit()
        
+    #
     textinput.update(pg.event.get())
-    screen.blit(textinput.get_surface(), (10,10))
+    screen.blit(textinput.get_surface(), (20,20))
 
+    # retrieves data from input textinput
     if textinput.update(events):
         print(textinput.get_text())
 
