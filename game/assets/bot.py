9d076c0e938ffd9f2392daa2e281341aa8c4161a
vivimouret29 <vivien.mouret@ynov.com>
2020-05-11 18:55:16 +0200

    :art: bot class migration

---
+++ game/assets/bot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import pygame
+
+pygame.font.init()
+
+# bot class
+class Bot(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.image = pygame.image.load('game/images/bluekappa.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 600
+        self.rect.y = 350
