d646fa4d39e1a06180f4be590c08fdff8153e3f9
vivimouret29 <vivien.mouret@ynov.com>
2020-05-11 18:56:50 +0200

    :zap: main file optimization

--- game/assets/main.py
+++ game/assets/main.py
@@ -2,38 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import pygame
+from game import Game
 
 pygame.font.init()
-
-# game class
-class Game:
-
-    def __init__(self):
-        self.player = Player()
-        self.bot = Bot()
-
-# player class
-class Player(pygame.sprite.Sprite):
-
-    def __init__(self):
-        super().__init__()
-        self.health = 100
-        self.max_health = 100
-        self.image = pygame.image.load('game/images/player.png')
-        self.rect = self.image.get_rect()
-        self.rect.x = -100
-        self.rect.y = 0
-
-class Bot(pygame.sprite.Sprite):
-
-    def __init__(self):
-        super().__init__()
-        self.health = 100
-        self.max_health = 100
-        self.image = pygame.image.load('game/images/bluekappa.png')
-        self.rect = self.image.get_rect()
-        self.rect.x = 600
-        self.rect.y = 350
 
 # creating windows
 screen = pygame.display.set_mode((800,600))
