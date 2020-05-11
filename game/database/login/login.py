d741c3bf80d3664e162c85c61a66647e6bdb4538
Hugo <hugosix33@gmail.com>
2020-05-11 18:41:31 +0200

    POO login.py

--- game/database/login/login.py
+++ game/database/login/login.py
@@ -2,29 +2,44 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import os.path
 
-connection = sqlite3.connect("../base.db")
-cursor = connection.cursor()
 
-pseudo = 'dsfsfs'
-password = 'secdret'
+class Login:
+    # Attribut
 
-query_pseudo = (pseudo,)
+    # Initialisateur
+    def __init__(self, pseudo, password):
+        self.pseudo = 'dfd'
+        self.password = 'secdret'
 
-requete = cursor.execute('SELECT * FROM users WHERE username = ?', query_pseudo)
-resultat = requete.fetchone()
+    def fetchPseudo(self):
+        query_pseudo = (self.pseudo,)  # mise en tuple
+        path_db = os.path.join(os.path.dirname(
+            os.path.abspath(__file__))) + '/../base.db'
+        connection = sqlite3.connect(path_db)
+        cursor = connection.cursor()
+        requete = cursor.execute(
+            'SELECT * FROM users WHERE username = ?', query_pseudo)
+        resultat = requete.fetchone()
+        connection.close()
+        return resultat
 
-if resultat != None:
-    req_pseudo, req_password = resultat[1], resultat[2]
+    def verify(self, resultat):
+        if resultat != None:
+            req_pseudo, req_password = resultat[1], resultat[2]
+            if self.pseudo == req_pseudo and self.password == req_password:
+                return "Login valide"
+                # Passer Login a TRUE
+            else:
+                return "Login invalide"
+                # Laisser Login a FALSE
+        else:
+            # On stop tout
+            pass
 
-    if pseudo == req_pseudo and password == req_password:
-        print("Login valide")
-        # Passer Login a TRUE
-    else:
-        print("Login invalide")
-        # Laisser Login a FALSE
-else:
-    # On stop tout
-    pass
 
-connection.close()
+# conn = Login('test', 'test')
+# res = conn.fetchPseudo()
+# print(res)
+# print(conn.verify(res))
