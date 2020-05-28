3e79dfe84f5cebef56a2ad2a0f3151b3b3d3a373
Thibault <thibaultfeugere@gmail.com>
2020-05-28 21:38:34 +0200

    Adding health bar system

--- game/assets/bot.py
+++ game/assets/bot.py
@@ -19,6 +19,16 @@ class Bot(pygame.sprite.Sprite):
         self.rect.x = 550
         self.rect.y = 250
 
+    def update_health_bar(self, surface):
+        bar_color = (111, 210, 46)
+        bar_position = [self.rect.x + 12, self.rect.y - 20, self.health, 5]
+
+        background_bar_color = (255, 0, 0)
+        background_bar_position = [self.rect.x + 12, self.rect.y - 20, self.max_health, 5]
+
+        pygame.draw.rect(surface, background_bar_color, background_bar_position)
+        pygame.draw.rect(surface, bar_color, bar_position)
+
     def heal(self, heal_amount):
         self.health += heal_amount
 
